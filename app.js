40a36a0c8216b6d4a4205e46432258bdecd828c3
MaTuTe248 <matutegnunez@com>
2023-11-13 00:38:27 -0300

    subiendo proyecto mercadoliebre al repositorio

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.listen(3030, () => console.log('Servidor Corriendo'));
+app.use(express.static('public'));
+
+app.get('/',(req,res) => res.sendFile(path.resolve("./views/index.html")));
+
+app.get('/register',(req,res) => res.sendFile(path.resolve("./views/registro.html")));
+
+app.get('/login',(req,res) => res.sendFile(path.resolve("./views/loggin.html")));
