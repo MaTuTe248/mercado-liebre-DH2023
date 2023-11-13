f6ebff6d5a5a78febe006e13ddd9c76847d202f0
MaTuTe248 <matutegnunez@com>
2023-11-13 20:14:21 -0300

    modiicando archivo jason

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const path = require("path");
 //Usando recursos estaticos
 app.use(express.static("public"));
 
+const port= process.env.PORT || 3000;
+
 //levantando el servidor en el puerto 3030
-app.listen(3030, () => console.log("Servidor Corriendo"));
+app.listen(port, () => console.log("Servidor Corriendo"));
 
 //** */ Rutas a los recursos
 
